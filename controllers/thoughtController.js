46c3f75bcd9fbea26ed8a157fd946ff17da5a4b0
berj ghazarian <ghazarian_berj@hotmail.com>
2022-05-02 17:14:23 -0700

    get thoughts

---
+++ controllers/thoughtController.js
@@ -0,0 +1,24 @@
+const { Thought, User } = require('../models');
+
+module.exports = {
+    getThoughts(req, res) {
+      Thought.find()
+        .populate('reactions')
+        .then((thoughts) => res.json(thoughts))
+        .catch((err) => res.status(500).json(err));
+    },
+  
+    getThoughtById(req, res) {
+      Thought.findOne({ _id: req.params.id })
+        .populate('reactions')
+        .select('-__v')
+        .then((thoughts) =>
+          !thoughts
+            ? res.status(404).json({ message: 'No User with that ID' })
+            : res.json({ thoughts })
+        )
+        .catch((err) => {
+          console.log(err);
+          return res.status(500).json(err);
+        });
+    },
