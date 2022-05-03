8717ff9501d7557acc9eb666c45bb4f444d8e103
berj ghazarian <ghazarian_berj@hotmail.com>
2022-05-02 17:10:05 -0700

    created reaction model on schema

--- models/thoughts.js
+++ models/thoughts.js
@@ -1,1 +1,28 @@
 const { Schema, Types, model } = require('mongoose');
+const reactionSchema = new Schema(
+    {
+      reactionId: {
+        type: Schema.Types.ObjectId,
+        default: () => new Types.ObjectId()
+      },
+      reactionBody: {
+        type: String,
+        require: true,
+        maxLength: 280
+      },
+      username: {
+        type: String,
+        require: true,
+      },
+      createdAt: {
+        type: Date,
+        default: Date.now,
+        get: (date) => Date(date).toLocaleDateString()
+  
+      }
+    },
+    {     toJSON: {
+        getters: true,
+      },
+    }
+  );
