d9084920ec7761a6323c7677151c31921c4cfcd9
berj ghazarian <ghazarian_berj@hotmail.com>
2022-05-02 17:10:34 -0700

    thought model created through schema

--- models/thoughts.js
+++ models/thoughts.js
@@ -26,3 +26,36 @@ const reactionSchema = new Schema(
       },
     }
   );
+  const thoughtSchema = new Schema(
+    {
+      thoughtText: {
+        type: String,
+        require: true,
+        minLength: 1,
+        maxLength: 280
+      },
+      reactionBody: {
+        type: String,
+        require: true,
+        maxLength: 280
+      },
+      createdAt: {
+        type: Date,
+        default: Date.now,
+        get: (date) => Date(date).toLocaleDateString()
+      },
+      username: {
+        type: String,
+        require: true,
+      },
+      reactions:
+        [reactionSchema],
+    },
+    {
+      toJSON: {
+        getters: true,
+        virtuals: true
+      },
+      id: false,
+    }
+  );
